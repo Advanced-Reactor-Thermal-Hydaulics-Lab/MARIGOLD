39840d1a8bd82162ad3e54f699d231e2cca27d3d
kang284 <david.kang0117@gmail.com>
2024-06-03 16:41:58 -0400

    Bug fix?

--- src/MARIGOLD/iate.py
+++ src/MARIGOLD/iate.py
@@ -67,7 +67,7 @@ def iate_1d_1g(
     else:
         theta       = parcel["theta"]
         Dh          = parcel["Dh"]
-        LoverD      = parcel["z_mesh"][-1]
+        LoverD      = parcel["z_mesh"][-1] / Dh
 
         rho_f       = parcel["rho_f"]
         rho_g       = parcel["rho_g"]
