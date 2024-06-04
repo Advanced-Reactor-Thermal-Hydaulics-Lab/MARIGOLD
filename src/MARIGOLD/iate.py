e6d6510914454e55da5a895de4b18f9ec87b87a6
kang284 <david.kang0117@gmail.com>
2024-06-04 11:36:09 -0400

    Changed elbow toggle to string argument for 'elbow', 'ubend', etc. Also possible fix to pressure, calculating gauge pressure instead of corrected gauge pressure

--- src/MARIGOLD/iate.py
+++ src/MARIGOLD/iate.py
@@ -11,23 +11,18 @@ def iate_1d_1g(
         dpdz_method = 'LM', cd_method = 'doe', void_method = 'driftflux',
 
         # Pressure drop calculation arguments
-        LM_C = 40, k_m = 0.40, LoverD_restriction = None,
+        LM_C = 40, k_m = 0.40,
 
         # Temporary arguments
-        elbow = False
+        restriction = None
         ):
     """ Calculate the area-averaged interfacial area concentration at query location based on the 1D 1G IATE
-
-    Version History:
-     - v1: Pressure cheating, jgref substitute for jgatm
-     - v2: MG update, pressure retrieval, jgatm retrieval
-     - v3: Yadav methods, incorporation of COV terms, support for elbows, VU, VD, horizontal
     
     Inputs:
      - cond:            Condition object, part of MARIGOLD framework
      - query:           L/D endpoint
      - z_step:          Axial mesh cell size [-]
-
+     - parcel:          Output package of iate_1d_1g(), can be used as input for subsequent runs
      - C_WE:            Wake entrainment coefficient
      - C_RC:            Random collision coefficient
      - C_TI:            Turbulent impact coefficient
@@ -37,20 +32,23 @@ def iate_1d_1g(
      - acrit_flag:      Enable/disable shutting off turbulence-based mechanisms beyond a critical void fraction
      - acrit:           Critical void fraction for shutting off turbulence-based mechanisms
      - C_inf:           Drift flux distribution parameter limiting value
-
      - dpdz_method:     Pressure drop prediction method, 'LM' or 'Kim'
      - cd_method:       Drag coefficient prediction method, 'iter' or 'doe'
      - void_method:     Void fraction prediction method, 'driftflux' or 'continuity'
+     - LM_C:            Lockhart-Martinelli Chisholm parameter
+     - k_m:             Minor loss coefficient
+     - restriction:     Restriction type, defaults to None, can be set to 'elbow', 'ubend'
 
     Notes:
      - Isn't this script nice and clean? Follow good coding practices, kids. - David
      - IATE coefficients set as optional inputs, with default values set depending on geometry
      - COV terms being implemented in Condition.py, not incorporated into this function yet
      - vgz calculation in elbow and dissipation length regions still need to be implemented
+     - Need a way to compute void fraction across restrictions, void fraction prediction falters
      - Modify MG for Yadav data extraction
     """
 
-    # MARIGOLD retrieval
+    # MARIGOLD retrieval and setup
     theta           = cond.theta                                # Pipe inclination angle
     Dh              = cond.Dh                                   # Hydraulic diameter
 
@@ -76,34 +74,22 @@ def iate_1d_1g(
     z_mesh = z_mesh * Dh                                        # Axial mesh [m], units necessary for dp calculation
     z_step = z_step * Dh
 
-    # Yadav
-    '''
-    RoverD          = 9                                         # Make into optional input
-    R_curv          = RoverD * Dh                               # Radius of curvature of elbow/bend
-    L_curv          = np.pi/2 * R_curv                          # This assumes 90\degree elbow    
-
-    # So, Yadav defines under Expcond.m the length of the vertical section -- I think this is how he toggles between VU, H, VD, elbow
-    # Instead, we probably would want to have an input switch indicating which mode of IATE we're executing
-    '''
-
     ############################################################################################################################
     #                                                                                                                          #
     #                                                      COEFFICIENTS                                                        #
     #                                                                                                                          #
     ############################################################################################################################
 
-    if theta == 90 and elbow == False:                          # Vertical, no elbow
-        if C_WE == None:
-            C_WE    = 0.002
-        if C_RC == None:
-            C_RC    = 0.004        
-        if C_TI == None:
-            C_TI    = 0.085
-
-        COV_RC      = 1
-        COV_TI      = 1
-
-    elif theta == 0 and elbow == False:                         # Horizontal, no elbow, look at Ran's work?
+    # For future reference, when adding other restriction types, want to maintain consistency in if/then logic.
+    #  1. Check angles with straight pipes
+    #  2. Check angles with restrictions
+    #  3. Check restrictions
+    #       a. 'elbow'
+    #       b. 'ubend'
+    #       c. 'dissipation'
+    #       d. Other
+    #  4. Else, default to vertical-upward
+    if theta == 0 and restriction == None:                      # Horizontal, no elbow, look at Ran's work?
         if C_WE == None:
             C_WE    = 0.000
         if C_RC == None:
@@ -114,7 +100,7 @@ def iate_1d_1g(
         COV_RC      = 1
         COV_TI      = 1
 
-    elif elbow == True:                                         # Elbow, look at Shoxu's work?
+    elif restriction == 'elbow':                                # Elbow, look at Shoxu's work?
         if C_WE == None:
             C_WE    = 0.000
         if C_RC == None:
@@ -125,7 +111,7 @@ def iate_1d_1g(
         COV_RC      = 1
         COV_TI      = 1
 
-    else:
+    else:                                                       # Default to vertical-upward, no elbow
         if C_WE == None:
             C_WE    = 0.002
         if C_RC == None:
@@ -188,13 +174,23 @@ def iate_1d_1g(
         jgloc       = parcel["jgloc"]
         jgatm       = parcel["jgatm"]
 
+    ########################################################################################################################
     # Pressure drop [Pa/m]
-    if LoverD_restriction == None:
-        L = None
-    else:
-        L = LoverD_restriction*Dh
+    if restriction == 'elbow':
+        delta_h = (z_mesh - z_mesh[0]) * 2 / np.pi              # The height of an elbow is going to be its radius
 
-    dpdz            = cond.calc_dpdz(method=dpdz_method, LM_C=LM_C, k_m=k_m, L=L)
+    elif restriction == 'ubend':
+        delta_h = 0
+
+    else:
+        delta_h = z_mesh - z_mesh[0]                            # Dissipation region is going to be the same as standard VU
+
+    dpdz = cond.calc_dpdz(
+        method = dpdz_method, 
+        LM_C = LM_C, 
+        k_m = k_m, 
+        L = (query - LoverD) * Dh
+        ) + rho_f * grav * delta_h
     
     # Local Pressure along the test section
     p = (jgatm * p_atm / jgloc) - p_atm                         # Back-calculate local corrected gauge pressure
@@ -264,13 +260,10 @@ def iate_1d_1g(
         # Estimate sources & sinks in the Interfacial Area Transport Eqn. (Part 1)
 
         # Sink due to Wake Entrainment
-        if theta == 90 and elbow == False:            
-            SWE[i] = C_WE * CDwe**(1/3) * ur * ai[i]**2 / 3 / np.pi
-
-        elif theta == 0 and elbow == False:
+        if theta == 0 and restriction == None:
             SWE[i] = 0
         
-        elif elbow == True:
+        elif restriction == 'elbow':
             SWE[i] = 0
             
             # Yadav calculates vgz differently if solving in elbow region and dissipation length region
@@ -293,6 +286,9 @@ def iate_1d_1g(
             Sratio = np.exp(-beta_diss * (z[i] - z[zstep_v+24]) / Dh)
             vgz[i] = vgzP4 * (1 + Const1 * np.log(Sratio))
             '''
+
+        else:
+            SWE[i] = C_WE * CDwe**(1/3) * ur * ai[i]**2 / 3 / np.pi
         
         # Sink due to Random Collisions	
         RC1 = ut * ai[i]**2 / alpha_max**(1/3) / (alpha_max**(1/3) - alpha[i]**(1/3))
