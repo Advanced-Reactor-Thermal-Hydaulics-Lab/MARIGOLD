Height calculation fix

src/MARIGOLD/iate.py
@@ -178,20 +178,20 @@ def iate_1d_1g(
     ########################################################################################################################
     # Pressure drop [Pa/m]
     if restriction == 'elbow':
-        delta_h = (z_mesh - z_mesh[0]) * 2 / np.pi              # The height of an elbow is going to be its radius
+        delta_h = (z_mesh - z_mesh[0]) * Dh * 2 / np.pi         # The height of an elbow is going to be its radius
 
     elif restriction == 'ubend':
         delta_h = 0
 
     else:
-        delta_h = z_mesh - z_mesh[0]                            # Dissipation region is going to be the same as standard VU
+        delta_h = (z_mesh - z_mesh[0]) * Dh                     # Dissipation region is going to be the same as standard VU
 
     dpdz = cond.calc_dpdz(
         method = dpdz_method, 
         LM_C = LM_C, 
         k_m = k_m, 
         L = (query - LoverD) * Dh
-        ) + rho_f * grav * delta_h
+        ) + (rho_f * grav * delta_h)
     
     # Local Pressure along the test section
     p = (jgatm * p_atm / jgloc) - p_atm                         # Back-calculate local corrected gauge pressure